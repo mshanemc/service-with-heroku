62fc99cce05c33d3063248bb9100a6830d4250ac
mshanemc <shane.mclaughlin@salesforce.com>
2019-08-28 17:15:49 -0500

    deployable with FS package and perms

--- orgInit.sh
+++ orgInit.sh
@@ -1,4 +1,6 @@
 sfdx force:org:create -f config/project-scratch-def.json -d 1 -s
-sfdx force:source:push
 sfdx force:package:install --package 04t0M000001MG7t -r --wait 20
+sfdx force:source:push
+sfdx force:user:permset:assign -n FSL_Admin_License
+sfdx force:user:permset:assign -n FSL_Admin_Permissions
 sfdx force:org:open
